a81489288275c8dda87111def41e105e3e45a2a2
fukingbus <kennethwu35@gmail.com>
2016-04-03 15:12:09 +0800

    download
    added download

--- index.js
+++ index.js
@@ -1,20 +1,69 @@
-var request = require('request-promise');
-var async = require('asyncawait/async');
-var await = require('asyncawait/await');
+const request = require('request-promise');
+const async = require('asyncawait/async');
+const await = require('asyncawait/await');
+const fs = require('fs');
+const pillow = require('sleep');
 
+var stickerOrigin;
 var main = async (function(){
-	console.log('main');
-	console.log(await (getLinks()));
-	
+	console.log('Fetching origin sticker links...');
+	stickerOrigin = await (getLinks());
+	scheduler();
 });
 var getLinks = async (function(){
-  var stickerId = '1033821';
-  var res = await( request('https://php-necroa.rhcloud.com/stickerline.php?stickerid='+stickerId)
+  stickerId = '1033821';
+  res = await( request('https://php-necroa.rhcloud.com/stickerline.php?stickerid='+stickerId)
   	.then(function (body){
 	  	//console.log(body);
 	  	return body;
-	 }));
+	 })
+	 .catch(function (err) {
+        console.log('ERROR OCCURED , retrying...');
+        main();
+    }));
 	 return (JSON.parse(res)).link;
 });
-
+var scheduler = async(function(){
+	for(var i=0,len=stickerOrigin.length;i<len;i++){
+		url = stickerOrigin[i];
+		filename = url.replace(/^.*[\\\/]/, '');
+		console.log('downloading '+filename + ' ('+(i+1)+'/'+len+' , '+((i/len)*100) +'%)');
+		try{
+			res = await(download(url,filename));
+		}
+		catch(e){
+			console.log('Error occured , retrying');
+			i--;
+		}
+	}
+});
+var download = async(function(url){
+	  	  res = await(request({
+            method: 'POST',
+            uri: 'http://waifu2x.udp.jp/api',
+            form: {
+                url: url,
+                style: 'art',
+                noise: 0,
+                scale: 2
+            },
+            encoding: null
+          }).then(function (body){
+			  	return saveImg(filename,body);
+			})
+			.catch(function (err) {
+		        throw err;
+		        return ;
+		    }));
+});
+var saveImg = (function(filename,data){
+		return (fs.writeFile(filename,data, (err) => {
+			  if (err) {
+			  	console.log( err);
+			  	return false;
+			 }
+			 console.log(filename + ' saved!');
+			 return true;
+		}));
+});
 main();
